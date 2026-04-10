1269423086eafcf39bbfc507baf33ac0bcb6bfe3
Chris-xiny <Xinycs@outlook.com>
2026-04-10 23:12:48 +0800

    Multi-Thread

--- week5/src/main/java/com/xin/MultiThread/BaoziShop.java
+++ week5/src/main/java/com/xin/MultiThread/BaoziShop.java
@@ -7,13 +7,39 @@ public class BaoziShop {
         this.count=count;
         this.flag=flag;
     }
-    public void getCount() {
+    public synchronized void getCount() {
+        if(flag==false){//如果没包子，线程等待
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //否则有包子，开始吃
         System.out.println("吃掉了第"+count+"个包子");
+        //改变flag为false，表示没包子了
+        flag=false;
+        //唤醒生产者包包子
+        notify();
     }
-    public void setCount() {
+
+    public synchronized void setCount() {
+        if(flag==true){//如果有包子，线程等待
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //否则没有包子，开始制作
         count++;
+        //改变flag为true，表示有包子了
+        flag=true;
+        //唤醒消费者吃包子
+        notify();
         System.out.println("生产了第"+count+"个包子");
     }
+    
     public boolean isFlag() {
         return flag;
     }
--- week5/src/main/java/com/xin/MultiThread/Consumer.java
+++ week5/src/main/java/com/xin/MultiThread/Consumer.java
@@ -9,25 +9,11 @@ public class Consumer implements Runnable {
     public void run(){
         while(true){//持续吃包子
             try {
-                Thread.sleep(100);//让程序不那么快
+                Thread.sleep(1000);//让程序不那么快
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            synchronized(bzs){
-                if(bzs.isFlag()==false){//如果没包子，线程等待
-                    try {
-                        bzs.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                //否则有包子，开始吃
-                bzs.getCount();
-                //改变flag为false，表示没包子了
-                bzs.setFlag(true);
-                //唤醒生产者包包子
-                bzs.notify();
-            }
+            bzs.getCount();
         }
     }
 }
