1565c8ecddc471ea2cf3fe3de9c21bc3d46e4300
Chris-xiny <Xinycs@outlook.com>
2026-04-11 22:44:46 +0800

    Multi-Thread0

---
+++ week5/src/main/java/com/xin/MultiThread02/Producer.java
@@ -0,0 +1,19 @@
+package com.xin.MultiThread02;
+
+public class Producer implements Runnable{
+    private BaoziShop bzs;
+    public Producer(BaoziShop bzs){
+        this.bzs=bzs;
+    }
+    @Override
+    public void run(){
+        while(true){//持续做包子
+            try {
+                Thread.sleep(1000);//让程序不那么快
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            bzs.setCount();
+        }
+    }
+}
---
+++ week5/src/main/java/com/xin/MultiThread02/test.java
@@ -0,0 +1,24 @@
+package com.xin.MultiThread02;
+
+public class test {
+    public static void main(String[] args) {
+        BaoziShop bzs =new BaoziShop(0, false);
+        Producer p=new Producer(bzs);
+        Consumer c=new Consumer(bzs);
+        //如果多个生产者和消费者会出现什么情况呢?
+        new Thread(p).start();
+        new Thread(p).start();
+        new Thread(p).start();
+        new Thread(c).start();
+        new Thread(c).start();
+        new Thread(c).start();
+        /*  
+            结果是不能做到生产一个消费一个了，原因是因为notify随机唤醒一个线程，举一个例子：假如flag==true生产者全部唤醒却都进入if等待后，
+            消费者线程才开始抢到锁，第一个吃掉了包子唤醒了生产线程，flag==true,但notify假如唤醒的还是生产者并且这个生产者抢到锁了，那么即使
+            flag==true，他依然会直接出if并生产，因为他上一次在if中wait，醒来抢到锁后直接继续上一次的执行
+            因此需要使用notifyAll()配合if改为while每次都要判断flag来解决问题
+        */
+    }
+    
+
+}
