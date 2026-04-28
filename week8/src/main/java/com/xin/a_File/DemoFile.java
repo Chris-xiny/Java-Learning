a19692d1229ae5428801e8cabf1eac821e6d8a34
Chris-xiny <Xinycs@outlook.com>
2026-04-28 18:21:33 +0800

    File

---
+++ week8/src/main/java/com/xin/a_File/DemoFile.java
@@ -0,0 +1,12 @@
+package com.xin.a_File;
+
+import java.io.File;
+
+public class DemoFile {
+    public static void main(String[] args) {
+        //static String pathSeparator:与系统有关的路径分隔符,为了方便,他被表示为一个字符串    ;
+        System.out.println(File.pathSeparator);
+        //static String Separator:与系统有关的默认名称分隔符,为了方便,他被表示为一个字符串    \ 
+        System.out.println(File.separator);
+    }
+}
