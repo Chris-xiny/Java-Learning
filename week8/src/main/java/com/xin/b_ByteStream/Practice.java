e1793008b76f9cf4ee09359e8fa87cf821910f0d
Chris-xiny <Xinycs@outlook.com>
2026-05-05 21:43:28 +0800

    FileInputStream_Practice

---
+++ week8/src/main/java/com/xin/b_ByteStream/Practice.java
@@ -0,0 +1,23 @@
+package com.xin.b_ByteStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) throws IOException {
+        System.out.println("请输入待复制文件路径");
+        Scanner sc=new Scanner(System.in);
+        String copyPath=sc.next();
+        System.out.println("请输入粘贴地址");
+        String pastePath=sc.next();
+        FileInputStream fin=new FileInputStream(copyPath);
+        FileOutputStream fout=new FileOutputStream(pastePath,true);
+        int len;
+        byte[] bytes=new byte[1024];
+        while((len=fin.read(bytes))!=-1){
+            fout.write(bytes,0,len);
+        }
+    }
+}
